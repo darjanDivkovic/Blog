99c94127f4603dc85287814d29374924555264a0
Darjan Divkovic <daarjan.tz@gmail.com>
2020-02-23 23:22:41 +0100

    Log in and register works, time to implement js validation!

--- script.js
+++ script.js
@@ -2,15 +2,32 @@
 $(document).ready(()=> {
 
     $('#register-box').hide();
+    $('#login-box').hide();
 
     $('#register-button').click(()=> {
         
         $('#register-box').show();
+        $('#login-box').hide();
 
+    });
+
+   
+
+    
+
+    $('#log-in-button').click(()=> {
+
+        $('#login-box').show();
+        $('#register-box').hide();
     });
 
     $('#X-button-register').click(()=> {
 
         $('#register-box').hide();
     });
+
+    $('#X-button-login').click(()=> {
+        $('#login-box').hide();
+    });
+    
 });
