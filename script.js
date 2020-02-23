2e315a6fdd52c094968d68fef1e60f1180562b7b
Darjan Divkovic <daarjan.tz@gmail.com>
2020-02-23 22:02:03 +0100

    .

---
+++ script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(()=> {
+
+    $('#register-box').hide();
+
+    $('#register-button').click(()=> {
+        
+        $('#register-box').show();
+
+    });
+
+    $('#X-button-register').click(()=> {
+
+        $('#register-box').hide();
+    });
+});
