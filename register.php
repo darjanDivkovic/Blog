f7f4be1503874903496277d351dca930199e65f1
Darjan Divkovic <daarjan.tz@gmail.com>
2020-02-25 12:24:12 +0100

    doin

--- register.php
+++ register.php
@@ -2,6 +2,13 @@
 
     require 'dbo.php';
 
-    
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
 
+    $result = $conn->query("SELECT username FROM users");
+
+    
 ?>
