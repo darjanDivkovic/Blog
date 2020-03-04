ba85cb2e16b3bb120a455445e6bff17ceba98fbb
Darjan Divkovic <daarjan.tz@gmail.com>
2020-03-04 11:41:06 +0100

    blah

--- admin/admin-home.php
+++ admin/admin-home.php
@@ -1,17 +1,18 @@
 <?php
-
     session_start();
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./admin-home-style.css">
     <title>Admin Homepage</title>
 </head>
 <body>
-    <h1>Welcome <span id="logged-username">daarjantz</span></h1>
+    <h1 id="welcome">Welcome  <span id="logged-username"><?php echo $_SESSION['username']; ?></span></h1>
+    
 </body>
 </html>
