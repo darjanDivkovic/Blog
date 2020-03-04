7f894352a8c05a8a19eb93abffe202856ca6388d
Darjan Divkovic <daarjan.tz@gmail.com>
2020-03-04 13:20:13 +0100

    meh

--- admin/admin-home.php
+++ admin/admin-home.php
@@ -14,10 +14,10 @@
 <body>
     <div id="status-bar">
         <h1 id="welcome">Welcome  <span><?php echo $_SESSION['username']; ?></span></h1>
-        
         <h1 id="status">status <span>Admin</span></h1>
+        <button id="changePassword">change password</button>
     </div>
+
     
-    <button id="changePassword">change password</button>
 </body>
 </html>
