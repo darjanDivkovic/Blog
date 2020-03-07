3bf5b307d49a94092baac79e5d3cedf67ad65d9e
Darjan Divkovic <daarjan.tz@gmail.com>
2020-03-07 12:22:41 +0100

    Change password box showing and hidding when desired now!

--- admin/admin-home.php
+++ admin/admin-home.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link rel="stylesheet" href="./admin-home-style.css">
     <title>Admin Homepage</title>
 </head>
