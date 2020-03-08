1f64d2848f0b16818505a1be0b6dbe74131c8d6a
Darjan Divkovic <daarjan.tz@gmail.com>
2020-03-08 17:20:36 +0100

    Log out option available!

--- admin/admin-home.php
+++ admin/admin-home.php
@@ -16,8 +16,16 @@
     <div id="status-bar">
         <h1 id="welcome">Welcome  <span><?php echo $_SESSION['username']; ?></span></h1>
         <h1 id="status">status <span>Admin</span></h1>
-        <button id="changePassword">CHANGE PASSWORD</button>
+        
     </div>
+
+    <div id="options">
+        <button class="options-button" id="changePassword">Change Password</button>
+        <form action="./log-out.php" method="get">
+        <button type="submit" class="options-button">Log out</button>
+        </form>
+    </div>
+    
 
     <div id="change-password-box">
         <form action="./change-password.php" method="POST">
